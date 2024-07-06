e1dd738c290394176f94658d0664725ed6db26e4
Dilyan Iliev <102140524+Dilyan-Iliev@users.noreply.github.com>
2024-07-06 18:25:33 +0300

    IBasketRepository added

---
+++ src/Microservices/Basket/Basket.API/Data/IBasketRepository.cs
@@ -0,0 +1,16 @@
+﻿using Basket.API.Models;
+
+namespace Basket.API.Data
+{
+    public interface IBasketRepository
+    {
+        //For GetBasketHandler
+        Task<ShoppingCart> GetBasket(string username, CancellationToken cancellationToken = default);
+
+        //For StoreBasketHandler
+        Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default);
+
+        //For DeleteBasketHandler
+        Task<bool> DeleteBasket(string username, CancellationToken cancellationToken= default);
+    }
+}
