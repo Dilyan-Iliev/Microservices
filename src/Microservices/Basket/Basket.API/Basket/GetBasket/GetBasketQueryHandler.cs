8725dfa48a025d7943f9d2e620f7e5d1fa7e6347
Dilyan Iliev <102140524+Dilyan-Iliev@users.noreply.github.com>
2024-07-06 12:30:39 +0300

    GetBasket handler and endpoint

--- src/Microservices/Basket/Basket.API/Basket/GetBasket/GetBasketQueryHandler.cs
+++ src/Microservices/Basket/Basket.API/Basket/GetBasket/GetBasketQueryHandler.cs
@@ -1,6 +1,19 @@
-﻿namespace Basket.API.Basket.GetBasket
+﻿using Basket.API.Models;
+using BuildingBlocks.CQRS;
+
+namespace Basket.API.Basket.GetBasket
 {
-    public class GetBasketQueryHandler
+    public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+    public record GetBasketResult(ShoppingCart ShoppingCart);
+
+
+    internal class GetBasketQueryHandler
+        : IQueryHandler<GetBasketQuery, GetBasketResult>
     {
+        public async Task<GetBasketResult> Handle(GetBasketQuery request,
+            CancellationToken cancellationToken)
+        {
+            return new GetBasketResult(new ShoppingCart("swn"));
+        }
     }
 }
