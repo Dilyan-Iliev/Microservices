2b0d84acc970229b22ff0c1e9891c8fc966b513a
Dilyan Iliev <102140524+Dilyan-Iliev@users.noreply.github.com>
2024-07-06 10:32:36 +0300

    GetProducts pagination

--- src/Microservices/Catalog/Catalog.API/Products/GetProducts/GetProductsQueryHandler.cs
+++ src/Microservices/Catalog/Catalog.API/Products/GetProducts/GetProductsQueryHandler.cs
@@ -1,10 +1,11 @@
 ﻿using BuildingBlocks.CQRS;
 using Catalog.API.Models;
 using Marten;
+using Marten.Pagination;
 
 namespace Catalog.API.Products.GetProducts
 {
-    public record GetProductsQuery() : IQuery<GetProductsResult>;
+    public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductsResult>;
     public record GetProductsResult(IEnumerable<Product> Products);
 
 
@@ -21,7 +22,8 @@ namespace Catalog.API.Products.GetProducts
         public async Task<GetProductsResult> Handle(GetProductsQuery request,
             CancellationToken cancellationToken)
         {
-            var products = await _session.Query<Product>().ToListAsync(cancellationToken);
+            var products = await _session.Query<Product>()
+                .ToPagedListAsync(request.PageNumber ?? 1, request.PageSize ?? 10, cancellationToken);
 
             return new GetProductsResult(products);
         }
