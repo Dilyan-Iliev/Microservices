740338f1d2ea0e2e8dcb31bf7e3965dc6d3e3d21
Dilyan Iliev <102140524+Dilyan-Iliev@users.noreply.github.com>
2024-07-07 15:11:39 +0300

    redis cache

---
+++ src/Microservices/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,58 @@
+﻿using Basket.API.Models;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Data
+{
+    public class CachedBasketRepository
+        : IBasketRepository
+    {
+        //this will be decorator class which adds additional functionallity to IBasketRepository runtime
+        //also this class acts as a proxy and forwards the calls to the underlying basket repository
+
+        private readonly IBasketRepository _repo;
+        private readonly IDistributedCache _cache;
+
+        public CachedBasketRepository(IBasketRepository repo, IDistributedCache cache)
+        {
+            _repo = repo;
+            _cache = cache;
+        }
+
+        public async Task<ShoppingCart> GetBasket(string username,
+            CancellationToken cancellationToken = default)
+        {
+            //Cache-asside:
+
+            //Try get from cache
+            var cachedBasket = await _cache.GetStringAsync(username, cancellationToken);
+            if (!string.IsNullOrEmpty(cachedBasket))
+            {
+                return JsonSerializer.Deserialize<ShoppingCart>(cachedBasket)!;
+            }
+
+            //If not in the cache - get from DB and then put in cache
+            var basket = await _repo.GetBasket(username, cancellationToken);
+            await _cache.SetStringAsync(username, JsonSerializer.Serialize(basket), cancellationToken);
+
+            return basket;
+        }
+
+        public async Task<ShoppingCart> StoreBasket(ShoppingCart basket,
+            CancellationToken cancellationToken = default)
+        {
+            await _repo.StoreBasket(basket, cancellationToken);
+            await _cache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket), cancellationToken);
+            return basket;
+        }
+
+        public async Task<bool> DeleteBasket(string username,
+            CancellationToken cancellationToken = default)
+        {
+            bool result = await _repo.DeleteBasket(username, cancellationToken);
+            await _cache.RemoveAsync(username, cancellationToken);
+
+            return result;
+        }
+    }
+}
--- src/Microservices/Basket/Basket.API/Program.cs
+++ src/Microservices/Basket/Basket.API/Program.cs
@@ -33,6 +33,13 @@ namespace Basket.API
             }).UseLightweightSessions();
 
             builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+            builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+            builder.Services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = builder.Configuration.GetConnectionString("Redis");
+            });
+
             builder.Services.AddExceptionHandler<CustomExcetpionHandler>();
 
             var app = builder.Build();
