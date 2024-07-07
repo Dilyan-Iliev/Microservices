894d7b4556fb5a291550e268c4af0867c38d50e5
Dilyan Iliev <102140524+Dilyan-Iliev@users.noreply.github.com>
2024-07-07 18:40:45 +0300

    discount.proto file

--- src/Microservices/Discount/Discount.gRPC/Program.cs
+++ src/Microservices/Discount/Discount.gRPC/Program.cs
@@ -1,5 +1,3 @@
-using Discount.gRPC.Services;
-
 namespace Discount.gRPC
 {
     public class Program
@@ -14,7 +12,6 @@ namespace Discount.gRPC
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            app.MapGrpcService<GreeterService>();
             app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
             app.Run();
