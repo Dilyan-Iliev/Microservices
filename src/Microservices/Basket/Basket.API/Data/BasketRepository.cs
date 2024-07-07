1eee4b1263555427071974b69d19f2a7f3d8b3a1
Dilyan Iliev <102140524+Dilyan-Iliev@users.noreply.github.com>
2024-07-07 10:24:46 +0300

    BasketRepository implemented

---
+++ src/Microservices/Basket/Basket.API/Data/BasketRepository.cs
@@ -0,0 +1,39 @@
+﻿using Basket.API.Exceptions;
+using Basket.API.Models;
+using Marten;
+
+namespace Basket.API.Data
+{
+    public class BasketRepository
+        : IBasketRepository
+    {
+        private readonly IDocumentSession _session;
+
+        public BasketRepository(IDocumentSession session)
+        {
+            _session = session;
+        }
+
+        public async Task<ShoppingCart> GetBasket(string username, CancellationToken cancellationToken = default)
+        {
+            var basket = await _session.LoadAsync<ShoppingCart>(username, cancellationToken);
+
+            return basket ?? throw new BasketNotFoundException(username);
+        }
+
+        public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+        {
+            _session.Store(basket);
+            await _session.SaveChangesAsync(cancellationToken);
+            return basket;
+        }
+
+        public async Task<bool> DeleteBasket(string username, CancellationToken cancellationToken = default)
+        {
+            _session.Delete<ShoppingCart>(username);
+            await _session.SaveChangesAsync(cancellationToken);
+            return true;
+
+        }
+    }
+}
