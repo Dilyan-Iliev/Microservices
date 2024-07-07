2ba4a225cd2fe443e164f96721d6d342469251c7
Dilyan Iliev <102140524+Dilyan-Iliev@users.noreply.github.com>
2024-07-07 10:33:23 +0300

    Setup DB connection string and Program.cs config

--- src/Microservices/Basket/Basket.API/Program.cs
+++ src/Microservices/Basket/Basket.API/Program.cs
@@ -1,5 +1,7 @@
+using Basket.API.Models;
 using BuildingBlocks.Behaviors;
 using Carter;
+using Marten;
 
 namespace Basket.API
 {
@@ -14,11 +16,19 @@ namespace Basket.API
 
             var assembly = typeof(Program).Assembly;
             builder.Services.AddCarter();
+
             builder.Services.AddMediatR(cfg =>
             {
                 cfg.RegisterServicesFromAssembly(assembly);
                 cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+                cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
             });
+
+            builder.Services.AddMarten(opts =>
+            {
+                opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+                opts.Schema.For<ShoppingCart>().Identity(x => x.UserName); //Id will be username
+            }).UseLightweightSessions();
 
             var app = builder.Build();
 
