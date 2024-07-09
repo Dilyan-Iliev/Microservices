933aa426b47b7d73cae9cc1578451a2c86b5bd94
Dilyan Iliev <102140524+Dilyan-Iliev@users.noreply.github.com>
2024-07-09 16:07:38 +0300

    Entity configurations and dbcontext creation

---
+++ src/Microservices/Ordering/Ordering.Infrastructure/Data/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects.StrongTypeIDs;
+
+namespace Ordering.Infrastructure.Data.Configurations
+{
+    public class CustomerConfiguration
+        : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id)
+                .HasConversion( //converts from CustomerId type into Guid to store it in DB
+                customerId => customerId.Value, //This lambda expression converts the CustomerId object to its underlying Guid value before storing it in the database
+                dbId => CustomerId.Of(dbId)); //This lambda expression converts the Guid value from the database back to a CustomerId objec
+
+            builder.Property(c => c.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(c => c.Email)
+                .HasMaxLength(255);
+
+            builder.HasIndex(c => c.Email).IsUnique();
+        }
+    }
+}
---
+++ src/Microservices/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects.StrongTypeIDs;
+
+namespace Ordering.Infrastructure.Data.Configurations
+{
+    public class OrderItemConfiguration
+        : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.HasKey(item => item.Id);
+            builder.Property(item => item.Id)
+                .HasConversion(
+                orderItemId => orderItemId.Value,
+                dbId => OrderItemId.Of(dbId));
+
+            builder.HasOne<Product>() //each orderItem relates to one product
+                .WithMany() //one product has many OrderItems
+                .HasForeignKey(item => item.ProductId);
+
+            builder.Property(item => item.Quantity).IsRequired();
+            builder.Property(item => item.Price).IsRequired();
+        }
+    }
+}
