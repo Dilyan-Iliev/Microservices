b5aa7141d7d22caf05100ca7b87c052aa4e4025b
Dilyan Iliev <102140524+Dilyan-Iliev@users.noreply.github.com>
2024-07-09 11:38:08 +0300

    Enums, Models, ValueObjects

---
+++ src/Microservices/Ordering/Ordering.Domain/Models/Order.cs
@@ -0,0 +1,37 @@
+﻿using Ordering.Domain.Abstractions;
+using Ordering.Domain.Enums;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Domain.Models
+{
+    //This is OrderAggregate - order aggregate root including all related entities and value objects
+    //Aggregate root is an entity that acts as entrypoint for the aggregate
+    public class Order : Aggregate<Guid>
+    {
+        //List of OrderItem entities
+        private readonly List<OrderItem> _orderItems = new();
+
+        public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
+
+        public Guid CustomerId { get; private set; } = default!;
+
+        public string OrderName { get; private set; } = default!;
+
+        //Value object
+        public Address ShippingAddress { get; private set; } = default!;
+
+        //Value object
+        public Address BillingAddress { get; private set; } = default!;
+
+        //Value object
+        public Payment Payment { get; private set; } = default!;
+
+        public OrderStatus Status { get; private set; } = OrderStatus.Pending;
+
+        public decimal TotalPrice
+        {
+            get => OrderItems.Sum(x => x.Price * x.Quantity);
+            private set { }
+        }
+    }
+}
---
+++ src/Microservices/Ordering/Ordering.Domain/ValueObjects/Address.cs
@@ -0,0 +1,14 @@
+﻿namespace Ordering.Domain.ValueObjects
+{
+    //Value object - it is immutable (we will not have setters and will use record type)
+    public record Address //related with custom address
+    {
+        public string FirstName { get; } = default!;
+        public string LastName { get; } = default!;
+        public string? EmailAddress { get; } = default!;
+        public string AddressLine { get; } = default!;
+        public string Country { get; } = default!;
+        public string State { get; } = default!;
+        public string ZipCode { get; } = default!;
+    }
+}
