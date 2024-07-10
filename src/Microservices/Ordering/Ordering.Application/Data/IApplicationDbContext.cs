3eaae671d293d5f2091ac8c30ce400f3a6b539e8
Dilyan Iliev <102140524+Dilyan-Iliev@users.noreply.github.com>
2024-07-10 12:30:07 +0300

    CreateOrder command and handler

---
+++ src/Microservices/Ordering/Ordering.Application/Data/IApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+
+namespace Ordering.Application.Data
+{
+    public interface IApplicationDbContext
+    {
+        //this is needed to inject the EFC DbContext in .Application layer,
+        //because Application layer does not have refference to .Infrastructure layer
+
+        DbSet<Customer> Customers { get; }
+        DbSet<Product> Products { get; }
+        DbSet<Order> Orders { get; }
+        DbSet<OrderItem> OrderItems { get; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ src/Microservices/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -0,0 +1,61 @@
+﻿using BuildingBlocks.CQRS;
+using Ordering.Application.Data;
+using Ordering.Application.Dtos;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects.StrongTypeIDs;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder
+{
+    public class CreateOrderCommandHandler
+        : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+    {
+        private readonly IApplicationDbContext _db;
+
+        public CreateOrderCommandHandler(IApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<CreateOrderResult> Handle(CreateOrderCommand request,
+            CancellationToken cancellationToken)
+        {
+            var order = CreateNewOrder(request.Order);
+
+            await _db.Orders.AddAsync(order, cancellationToken);
+            await _db.SaveChangesAsync(cancellationToken);
+
+            return new CreateOrderResult(order.Id.Value);
+        }
+
+        private Order CreateNewOrder(OrderDto orderDto)
+        {
+            //create value objects
+            var shippingAddress = Address.Of(orderDto.ShippingAddress.FirstName, orderDto.ShippingAddress.LastName, orderDto.ShippingAddress.EmailAddress, orderDto.ShippingAddress.AddressLine, orderDto.ShippingAddress.Country, orderDto.ShippingAddress.State, orderDto.ShippingAddress.ZipCode);
+            var billingAddress = Address.Of(orderDto.BillingAddress.FirstName, orderDto.BillingAddress.LastName, orderDto.BillingAddress.EmailAddress, orderDto.BillingAddress.AddressLine, orderDto.BillingAddress.Country, orderDto.BillingAddress.State, orderDto.BillingAddress.ZipCode);
+            var payment = Payment.Of(orderDto.Payment.CardName, orderDto.Payment.CardNumber, orderDto.Payment.Expiration, orderDto.Payment.Cvv, orderDto.Payment.PaymentMethod);
+            var orderId = OrderId.Of(Guid.NewGuid());
+            var customerId = CustomerId.Of(orderDto.CustomerId);
+            var orderName = OrderName.Of(orderDto.OrderName);
+
+            //create order
+            var newOrder = Order.Create(
+                    id: orderId,
+                    customerId: customerId,
+                    orderName: orderName,
+                    shippingAddress: shippingAddress,
+                    billingAddress: billingAddress,
+                    payment: payment
+                    );
+
+            //add order items
+            foreach (var orderItemDto in orderDto.OrderItems)
+            {
+                newOrder
+                    .Add(ProductId.Of(orderItemDto.ProductId), orderItemDto.Quantity, orderItemDto.Price);
+            }
+
+            return newOrder;
+        }
+    }
+}
