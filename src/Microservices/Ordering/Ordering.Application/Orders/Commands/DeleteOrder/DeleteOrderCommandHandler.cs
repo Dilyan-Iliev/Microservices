459c2c3d56bd005e4b9f6033854f27cf622f2f47
Dilyan Iliev <102140524+Dilyan-Iliev@users.noreply.github.com>
2024-07-10 13:03:34 +0300

    DeleteOrder command and handler

---
+++ src/Microservices/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using BuildingBlocks.CQRS;
+using Ordering.Application.Data;
+using Ordering.Application.Exceptions;
+using Ordering.Domain.ValueObjects.StrongTypeIDs;
+
+namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandHandler
+        : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+    {
+        private readonly IApplicationDbContext _db;
+
+        public DeleteOrderCommandHandler(IApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<DeleteOrderResult> Handle(DeleteOrderCommand request,
+            CancellationToken cancellationToken)
+        {
+            var orderId = OrderId.Of(request.OrderId);
+            var order = await _db.Orders
+                .FindAsync([orderId], cancellationToken);
+
+            if (order == null)
+            {
+                throw new OrderNotFoundException(request.OrderId);
+            }
+
+            _db.Orders.Remove(order);
+            await _db.SaveChangesAsync(cancellationToken);
+
+            return new DeleteOrderResult(true);
+        }
+    }
+}
