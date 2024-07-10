4c24200edfd87ad67a7a97915b13a2e268d27cc6
Dilyan Iliev <102140524+Dilyan-Iliev@users.noreply.github.com>
2024-07-10 17:45:24 +0300

    wip

--- src/Microservices/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Microservices/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,14 +1,27 @@
-﻿namespace Ordering.API
+﻿using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace Ordering.API
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration config)
         {
+            services.AddExceptionHandler<CustomExcetpionHandler>();
+            services.AddHealthChecks()
+                .AddSqlServer(config.GetConnectionString("Database")!);
+
             return services;
         }
 
         public static WebApplication UseApiServices(this WebApplication app)
         {
+            app.UseExceptionHandler(options => { });
+            app.UseHealthChecks("/health", new HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
 
             return app;
         }
