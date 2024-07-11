4af13cb0d057cdf4a0c10dee9d41759205ac7737
Dilyan Iliev <102140524+Dilyan-Iliev@users.noreply.github.com>
2024-07-11 18:35:58 +0300

    IntegrationEvent

---
+++ src/Common/BuildingBlocks.Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace BuildingBlocks.Messaging.Events
+{
+    //Base class which provide common propertis to all others integration events
+    public record IntegrationEvent
+    {
+        public Guid Id => Guid.NewGuid();
+
+        public DateTime OccuredOn = DateTime.Now;
+
+        public string EventType => GetType().AssemblyQualifiedName!;
+    }
+}
