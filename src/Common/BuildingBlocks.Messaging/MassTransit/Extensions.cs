d0b08f2129d92fe8994e5288f084463b986f35e7
Dilyan Iliev <102140524+Dilyan-Iliev@users.noreply.github.com>
2024-07-11 18:52:44 +0300

    MassTransit with RabbitMQ configuration

---
+++ src/Common/BuildingBlocks.Messaging/MassTransit/Extensions.cs
@@ -0,0 +1,43 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BuildingBlocks.Messaging.MassTransit
+{
+    //Extension methods for setting up MassTransit with RabbitMQ
+    public static class Extensions
+    {
+        public static IServiceCollection AddMessageBroker(this IServiceCollection services,
+            IConfiguration configuration, Assembly? assembly = null)
+        {
+            //RabbitMQ MassTransit config
+            services.AddMassTransit(config =>
+            {
+                //sets the naming convetion for the endpoints (e.g., order-service instead of OrderService)
+                config.SetKebabCaseEndpointNameFormatter(); 
+
+                if (assembly != null)
+                {
+                    config.AddConsumers(assembly);
+                }
+
+                config.UsingRabbitMq((context, configurator) =>
+                {
+                    configurator.Host(new Uri(configuration["MessageBroker:Host"]!),
+                        host =>
+                        {
+                            host.Username(configuration["MessageBroker:UserName"]!);
+                            host.Password(configuration["MessageBroker:Password"]!);
+                        });
+
+                    //MassTransit automatically configures the endpoints for the consumers
+                    configurator.ConfigureEndpoints(context);
+                });
+            });
+
+
+            return services;
+        }
+    }
+}
