e8233664fb13b23c0af08447b808f7e34137a993
Dilyan Iliev <102140524+Dilyan-Iliev@users.noreply.github.com>
2024-07-14 15:00:17 +0300

    rate limiting for single endpoint

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 namespace YarpApiGateway
 {
     public class Program
@@ -9,8 +11,20 @@ namespace YarpApiGateway
             builder.Services.AddReverseProxy()
                 .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+            //control the amount of traffic a service can handle
+            //rate limiting can be configured per route or globally across all routes
+            builder.Services.AddRateLimiter(rateLimiterOpts =>
+            {
+                rateLimiterOpts.AddFixedWindowLimiter("fixed", options =>
+                {
+                    options.Window = TimeSpan.FromSeconds(5);
+                    options.PermitLimit = 5;
+                });
+            });
+
             var app = builder.Build();
 
+            app.UseRateLimiter();
             app.MapReverseProxy();
 
             app.Run();
